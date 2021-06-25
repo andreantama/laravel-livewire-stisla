78d11ed04b2e92ef7bc5325fef695fc002055e5d
Andrean Tama <andreanyogatama1@gmail.com>
2021-06-24 22:34:37 -0700

    error validation for register

--- app/Http/Livewire/Admin/Register.php
+++ app/Http/Livewire/Admin/Register.php
@@ -25,6 +25,14 @@ class Register extends Component
     public function store()
     {
         $this->validate();
+        $this->emit('msg',  'Berhasil', 'Anda telah berhasil mendaftarkan akun anda', 'success');
+        $this->reset();
+    }
+    public function dehydrate()
+    {
+        if(count($this->getErrorBag()->all()) > 0){
+            $this->emit('msg',  'Terjadi Kesalahan', 'Silahkan lengkapi semua form yang tersedia', 'error');
+        }
     }
     public function render()
     {
